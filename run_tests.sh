fb3fdf86670d050d25d38113675bc15496d3d9ef
Ivan Litov <ivanslitov@gmail.com>
2024-01-06 13:39:52 +0200

    Update tests run method from custom target to ctest

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,8 @@
-cmake --build ./build --config Release --target run_median_tests
+cmake --build ./build --config Release --target median_test
+
+if test $? -eq 0
+then
+    cd ./build && ctest
+else
+    echo "Build failed"
+fi
