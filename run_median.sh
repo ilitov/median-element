13e4e3a48ace1c66c58c50f362d29f7df16b114e
Ivan Litov <ivanslitov@gmail.com>
2024-01-06 16:30:57 +0200

    Update run_median script and cmake file

--- run_median.sh
+++ run_median.sh
@@ -1,1 +1,9 @@
-cmake --build ./build --config Release --target run_median
+cmake --build ./build --config Release --target median
+
+if test $? -eq 0
+then
+    echo "Running main executable..."
+    ./build/median
+else
+    echo "Build failed"
+fi
