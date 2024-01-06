7e54a750efc739dd2fc2a24b4d05c9ff78e17ccd
Ivan Litov <ivanslitov@gmail.com>
2024-01-06 16:30:22 +0200

    Update main executable

--- main.cpp
+++ main.cpp
@@ -1,34 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <random>
-#include <chrono>
 
 #include "Median.hpp"
 
 int main() {
-    std::size_t count = 100'000'000;
+    std::cout << "Enter the total number of elements:\n";
 
-    std::cout << "Enter number of elements: \n";
-   // std::cin >> count;
+    int elmntCount;
+    std::cin >> elmntCount;
 
-    std::random_device rd;
-    std::mt19937 gen{ rd() };
-    std::uniform_int_distribution<> distrib{  std::numeric_limits<int>::min(), std::numeric_limits<int>::max() };
+    Median med;
 
-    std::cout << "Generating " << count << " random elements\n";
+    for (int i = 0; i < elmntCount; ++i) {
+        std::cout << "Enter number[" << i + 1 << '/' << elmntCount << "]: ";
 
-    const auto start = std::chrono::steady_clock::now();
-    {
-        Median med;
-        for (size_t i = 0; i < count; ++i) {
-            med.add(distrib(gen));
-        }
+        int num;
+        std::cin >> num;
 
-        std::cout << "Median is " << med.median() << '\n';
+        med.add(num);
+        std::cout << "Current median = " << med.median() << '\n';
     }
-    const auto end = std::chrono::steady_clock::now();
-
-    std::cout << "Time = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms\n";
 
     return 0;
 }
