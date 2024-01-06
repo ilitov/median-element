fb5f7952932cdabc3e8459fc2b792345a7007b1c
Ivan Litov <ivanslitov@gmail.com>
2024-01-06 15:57:48 +0200

    Add comments to CustomHeap

--- include/CustomHeap.hpp
+++ include/CustomHeap.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 
+// Heap data structure that supports push() and replaceTop(), but does not support pop()
 template <typename T, typename Comp>
 class CustomHeap {
 public:
@@ -15,15 +16,20 @@ public:
 		return m_data.empty();
 	}
 
+	// Returns the top element of the heap
+	// It is undefined behaviour to call this method if the heap is empty
 	const T& top() const {
 		return m_data.front();
 	}
 
+	// Adds new element to the heap
 	void push(const T &value) {
 		m_data.push_back(value);
 		std::push_heap(m_data.begin(), m_data.end(), m_comp);
 	}
 
+	// Replaces the top element of the heap with the provided value and returns the old top element
+	// It is undefined behaviour to call this method if the heap is empty
 	T replaceTop(const T &value) {
 		const T oldTop = m_data.front();
 		m_data.front() = value;
@@ -39,13 +45,15 @@ private:
 		std::size_t lChild = leftChild(idx);
 
 		while (lChild < size) {
+			const std::size_t rChild = lChild + 1;
 			std::size_t next = lChild;
-			std::size_t rChild = lChild + 1;
-			
+
+			// Continue with the right child if it comes before the left child according to the comparator
 			if (rChild < size && !m_comp(m_data[rChild], m_data[lChild])) {
 				next = rChild;
 			}
 
+			// Exit the loop if the selected child comes after the current root according to the comparator
 			if (m_comp(m_data[next], m_data[idx])) {
 				return;
 			}
